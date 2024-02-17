b00787c678103b6c87f260ba4edb215f2780dbd0
kirank22 <45580946+kirank22@users.noreply.github.com>
2024-02-17 01:33:14 -0500

    swifter and twitterapikit integrations

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import SwifteriOS
+import TwitterAPIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "eJI7WV4CN0QZpr53pDgsZkKKx", consumerSecret: "f7yEWHgtEbw7JmztyH070bP1XUBE3UkTDFhNViMMuPRzJDIgrj")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@Apple") { (results, searchMetadata) in
+            print(results)
+        } failure: { error in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
